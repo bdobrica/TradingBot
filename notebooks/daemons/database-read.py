284b6d0672132356d07e0200e6da1b4124050ba4
Bogdan Dobrica <bdobrica@gmail.com>
2020-10-27 19:27:42 +0200

    Update database-read.py
    - the database read script;
    - it allows reading data for trends, profits and orders;

--- notebooks/daemons/database-read.py
+++ notebooks/daemons/database-read.py
@@ -1,33 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import datetime
 import pika
 import json
 import pandas as pd
 from rabbitmq import Subscriber
+from rabbitmq import Publisher
+from config import app_config
+from db import mk_schema, OrderStatus
 
-from sqlalchemy import create_engine, MetaData, Table, Column, Index
-from sqlalchemy.types import BigInteger, Float, String, DateTime, Float
-from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.sql.expression import Insert
-
-# adds the word IGNORE after INSERT in sqlalchemy
-@compiles(Insert)
-def _prefix_insert_with_ignore(insert, compiler, **kwords):
-    return compiler.visit_insert(insert.prefix_with('IGNORE'), **kwords)
+from sqlalchemy import create_engine, MetaData
 
 meta = MetaData()
-table = Table(
-    'transactions', meta, 
-    Column('id', BigInteger, primary_key = True),
-    Column('price', Float), 
-    Column('symbol', String(32)),
-    Column('time', DateTime),
-    Column('stamp', BigInteger),
-    Column('volume', Float)
-)
-_ = Index('symbol', table.c.symbol)
-_ = Index('symbol_stamp', table.c.symbol, table.c.stamp, unique = True)
-
-engine = create_engine('mysql://root:raspberry@localhost/tradingbot')
+mk_schema(meta)
+engine = create_engine('{db.driver}://{db.username}:{db.password}@{db.host}/{db.database}'.format(db = app_config.db))
 meta.create_all(engine)
 
 class DbSubscriber(Subscriber):
@@ -44,11 +29,166 @@ class DbSubscriber(Subscriber):
             super().__getitem__(key)
     
     def _send_profits(self):
-        pass
+        current_stamp = int(datetime.datetime.now(tz = datetime.timezone.utc).timestamp() * 1000)
+        orders = pd.read_sql('select\
+            count(*) as active_orders\
+        from\
+            orders\
+        where\
+            stamp <= %(stamp)s and\
+            status in %(status)s;',
+            con = engine,
+            params = {
+                'stamp': current_stamp,
+                'status': [OrderStatus.PENDING, OrderStatus.PARTIAL]
+            }
+        )
+        budget = pd.read_sql('select\
+            amount,\
+            stamp\
+        from\
+            budget\
+        order by\
+            stamp desc\
+        limit 1;', con = engine)
+        portfolio = pd.read_sql('select\
+            symbol,\
+            sum(commission) as commission,\
+            sum(price * volume) as value,\
+            sum(volume) as volume\
+            max(stamp) as stamp\
+        from\
+            portfolio\
+        group by\
+            symbol;',
+            con = engine)
+        prices = pd.read_sql('select\
+            A.symbol,\
+            A.price,\
+            A.stamp\
+        from\
+            transactions A inner join\
+            (select\
+                symbol,\
+                max(stamp) as stamp\
+            from\
+                transactions\
+            group by\
+                symbol\
+            ) B on (A.symbol = B.symbol and A.stamp = B.stamp);',
+            con = engine)
+        message = {
+            'stamp': current_stamp,
+            'active_orders': orders['active_orders'][0],
+            'budget': {
+                'amount': budget['amount'][0],
+                'stamp': budget['stamp'][0]
+            },
+            'portfolio': portfolio.to_dict(),
+            'prices': prices.to_dict()
+        }
+        self.publisher['routing_key'] = 'requested.profit'
+        self.publisher.publish(message)
+        
     def _send_trends(self, lookahead, lookbehind):
-        pass
+        current_stamp = int(datetime.datetime.now(tz = datetime.timezone.utc).timestamp() * 1000)
+        begin_stamp = current_stamp - lookbehind * 1000
+        end_stamp = current_stamp - lookahead * 1000
+        orders = pd.read_sql('select\
+            count(1) as active_orders\
+        from\
+            orders\
+        where\
+            stamp <= %(stamp)s and\
+            status in %(status)s;',
+            con = engine,
+            params = {
+                'stamp': current_stamp,
+                'status': [OrderStatus.PENDING, OrderStatus.PARTIAL]
+            }
+        )
+        budget = pd.read_sql('select\
+            amount,\
+            stamp\
+        from\
+            budget\
+        order by\
+            stamp desc\
+        limit 1;', con = engine)
+        symbols = pd.read_sql('select\
+            symbol\
+        from\
+            transactions\
+        where\
+            stamp >= %(begin)s and\
+            stamp < %(end)s\
+        group by\
+            symbol;',
+            con = engine,
+            params = {
+                'begin': begin_stamp,
+                'end': end_stamp
+            }
+        )
+        transactions = pd.read_sql('select\
+            id,\
+            price,\
+            symbol,\
+            stamp,\
+            volume\
+        from\
+            transactions\
+        where\
+            stamp >= %(begin)s and\
+            stamp < %(end)s;',
+            con = engine,
+            params = {
+                'begin': begin_stamp,
+                'end': end_stamp
+            }
+        )
+        message = {
+            'stamp': current_stamp,
+            'active_orders': orders['active_orders'][0],
+            'budget': {
+                'amount': budget['amount'][0],
+                'stamp': budget['stamp'][0]
+            },
+            'symbols': symbols.to_dict(),
+            'transactions': transactions.to_dict()
+        }
+        self.publisher['routing_key'] = 'requested.trends'
+        self.publisher.publish(message)
+    
     def _send_orders(self, lookahead):
-        pass
+        # first, substract the lookahead time from the current time,
+        # cause this is the delay between a order is made and when it is processed
+        current_stamp = int(datetime.datetime.now(tz = datetime.timezone.utc).timestamp() * 1000)
+        order_stamp = current_stamp - lookahead * 1000
+        orders = pd.read_sql('select\
+            id,\
+            price,\
+            symbol,\
+            stamp,\
+            volume,\
+            status\
+        from\
+            orders\
+        where\
+            stamp <= %(stamp)s and\
+            status in %(status)s;',
+            con = engine,
+            params = {
+                'stamp': order_stamp,
+                'status': [OrderStatus.PENDING, OrderStatus.PARTIAL]
+            }
+        )
+        message = {
+            'stamp': current_stamp,
+            'orders': orders.to_dict()
+        }
+        self.publisher['routing_key'] = 'requested.orders'
+        self.publisher.publish(message)
     
     def on_message_callback(self, basic_delivery, properties, body):
         body_object = json.loads(body)
@@ -83,6 +223,9 @@ params = pika.ConnectionParameters(host='localhost')
 subscriber = DbSubscriber(params)
 subscriber['queue'] = 'database'
 subscriber['routing_key'] = 'database.read'
-        
+publisher = Publisher(params)
+publisher['queue'] = 'requested'
+subscriber['publisher'] = publisher
+
 if __name__ == '__main__':
     subscriber.run()
