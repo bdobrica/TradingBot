9c84f5af56b29f6ab217606bbe222615b976a5f4
Bogdan Dobrica <bdobrica@gmail.com>
2020-11-01 21:36:04 +0200

    Update check-trends.py
    - simple trend checking by adding a tensorflow linear regression model;

--- notebooks/daemons/check-trends.py
+++ notebooks/daemons/check-trends.py
@@ -7,8 +7,13 @@ from rabbitmq import Subscriber
 from rabbitmq import Publisher
 from config import app_config
 from db import mk_schema, OrderStatus
-
 from sqlalchemy import create_engine, MetaData
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+import numpy as np
 
 meta = MetaData()
 mk_schema(meta)
@@ -39,10 +44,51 @@ class CheckTrendsSubscriber(Subscriber):
         self.publisher.publish(message)
     
     def _compute_trend(self, transactions):
-        pass
-    
+        # extract the features
+        features = transactions[['stamp', 'volume']].values
+        # make the stamps more manageble
+        features[:,0] -= min(features[:,0])
+        features[:,0] /= 3600 * 1000
+        # extract the outputs
+        prices = transactions['price'].values
+        # use a tensorflow normalizer for the inputs
+        normalizer = preprocessing.Normalization(input_shape = [2,])
+        normalizer.adapt(features)
+        # as models work best with small number, outputs will also be normalized
+        mu_price = np.mean(prices)
+        sigma_price = np.mean(prices)
+        # build a linear regression model
+        model = tf.keras.Sequential([
+            normalizer,
+            layers.Dense(1, activation = 'linear'),
+            layers.Lambda(lambda output : output * sigma_price + mu_price)
+        ])
+        # choose an optimizer and a loss
+        model.compile(
+            optimizer='adam',
+            loss='mean_absolute_error'
+        )
+        # fit the model
+        model.fit(
+            features,
+            prices,
+            validation_split = 0.2,
+            epochs = 5,
+            verbose = 0
+        )
+        # evaluate the trend
+        predicted = model.predict([
+            [features[0,0], 0.0],
+            [features[-1,0], 0.0]
+        ]).squeeze()
+        # compute the slope of the trend
+        trend = (predicted[1] - predicted[0]) / (features[-1,0] - features[0,0])
+        
+        return trend
+        
     def _distribute_budget(self, orders, budget):
-        pass
+        amount = budget['amount']
+        
     
     def on_message_callback(self, basic_delivery, properties, body):
         body_object = json.loads(body)
@@ -59,11 +105,6 @@ class CheckTrendsSubscriber(Subscriber):
         budget = body_object['budget']
         if budget['amount'] <= 0:
             return
-        if 'symbols' not in body_object:
-            return
-        symbols = pd.DataFrame.from_dict(body_object['symbols'])
-        if symbols.shape[0] == 0:
-            return
         if 'transactions' not in body_object:
             return
         transactions = pd.DataFrame.from_dict(body_object['transactions'])
@@ -71,7 +112,7 @@ class CheckTrendsSubscriber(Subscriber):
             return
         
         orders = pd.DataFrame(columns = ['symbol', 'volume', 'trend'])
-        for symbol in symbols.iterrows():
+        for symbol in transactions['symbol'].unique():
             symbol_transactions = transactions[transactions['symbol'] == symbol]
             trend = self._compute_trend(symbol_transactions)
             orders = orders.append({
