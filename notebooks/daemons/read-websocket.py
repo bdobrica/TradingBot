1957c10012bde5cdd76afe45c855b1e6ff1f7406
Bogdan Dobrica <bdobrica@gmail.com>
2020-11-19 19:08:46 +0200

    Update read-websocket.py
    - fixed app_config.log.levels to app_config.log.level;
    - updated the queue as the routing key is not enough for message routing (should check the on_message_callback properties);

--- notebooks/daemons/read-websocket.py
+++ notebooks/daemons/read-websocket.py
@@ -13,7 +13,7 @@ from rabbitmq import Publisher # pylint: disable=import-error
 params = pika.ConnectionParameters(host='localhost')
 publisher = Publisher(params)
 # the queue is "database", the routing key is "database.save"
-publisher['queue'] = 'database'
+publisher['queue'] = 'database_save'
 publisher['routing_key'] = 'database.save'
 
 # create the template container for transactions
@@ -21,7 +21,7 @@ df = pd.DataFrame(columns = ['price', 'symbol', 'stamp', 'volume'])
 
 # initialize the logger so we see what happens
 logger_path = Path(app_config.log.path)
-logger = Logger(path = logger_path / Path(__file__).stem, level = int(app_config.log.levels))
+logger = Logger(path = logger_path / Path(__file__).stem, level = int(app_config.log.level))
 
 def on_message(ws, message):
     """
